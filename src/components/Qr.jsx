69bc13fc2acf28ed2a6eed6939d90c10bcaa0b77
Arleen Alcaraz-Cano <aaa2428@barnard.edu>
2023-10-19 11:33:45 -0400

    updated style and Qr.jsx

--- src/components/Qr.jsx
+++ src/components/Qr.jsx
@@ -1,16 +1,24 @@
-import React from 'react'
+import React from 'react';
 
 export default function Qr() {
-    return (
-        <body>
-            <div id = "container">
-                <img id = "QRCode" src="image-qr-code.png" alt="QR Code Image"/> 
-                <div id = "text">
-                    <h1 class="bolded"> Improve your front-end  <br /> skills by building projects <br/> </h1>
-                    <p class="notBolded"> Scan the QR code to visit Frontend <br /> 
-                    Mentor and take your coding skills <br /> to the next level </p>
-                </div>
-            </div> 
-        </body>
-    )
+  return (
+    <React.Fragment>
+    <body class="bg-blue-200 flex justify-center items-center h-[110vh]">
+        <div id="container" className="flex flex-col items-center w-[40vh]" style={{ height: '60vh' }}>
+        <img id="QRCode" src="image-qr-code.png" alt="QR Code Image" className="w-[90h] rounded-lg mt-5 mb-10"/>
+        <div id="text">
+          <h1 className="text-lg font-bold text-center">
+            Improve your front-end <br /> 
+            skills by building projects <br />
+          </h1>
+          <p className="text-sm mt-5 text-center text-gray-600">
+            Scan the QR code to visit Frontend <br />
+            Mentor and take your coding skills <br />
+            to the next level
+          </p>
+        </div>
+      </div>
+    </body>
+    </React.Fragment>
+  );
 }
